556756ec42d9fed6c82f194c2abc49620d5c7a41
Jean-François Tronche <jftronche@univ-orleans.fr>
2019-05-10 15:45:48 +0200

    Finito el scripto

--- structure.py
+++ structure.py
@@ -2,12 +2,25 @@
 import os
 import json
 
+def print_hierarchie(fichier_json):
+    with open(fichier_json,'r') as f:
+        structure = json.load(f)
+        print('La hiérarchie existe déjà')
+        print('Celle-ci est la suivante :')
+        for keys,values in structure.items():
+            print('. {0}'.format(key))
+            for value in values:
+                print('--- {0}'.format(value))
+
+                print('='*25)
+
 def creer_dossiers(dossiers):
     for key,values in dossiers.items():
         for value in values:
             dossier = '{0}/{1}/{2}'.format(base,key,value)
             os.makedirs(dossier)
             print('Création du dossier {0}'.format(dossier))
+
 def creer_json(fichier_json,dictionnaire):
     with open(fichier_json, 'w') as f:
         json.dump(dictionnaire,f,indent=4)
@@ -22,5 +35,9 @@ structure = {
             'videos': ['chats','hamsters','concombres']
             }
 
-creer_dossiers(structure)
-creer_json(fichier_json,structure)
+
+if os.path.isfile(fichier_json):
+    print_hierarchie(fichier_json)
+else:
+    creer_dossiers(structure)
+    creer_json(fichier_json, structure)
