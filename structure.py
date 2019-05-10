1a6f46f5df4dcdcccef9b7b34e12d9a3651767c2
Jean-François Tronche <jftronche@univ-orleans.fr>
2019-05-10 15:05:54 +0200

    Add file

--- structure.py
+++ structure.py
@@ -1,6 +1,6 @@
 # création d'une structure de dossiers
 import os
-
+import json
 
 def creer_dossiers(dossiers):
     for key,values in dossiers.items():
@@ -8,9 +8,14 @@ def creer_dossiers(dossiers):
             dossier = '{0}/{1}/{2}'.format(base,key,value)
             os.makedirs(dossier)
             print('Création du dossier {0}'.format(dossier))
+def creer_json(fichier_json,dictionnaire):
+    with open(fichier_json, 'w') as f:
+        json.dump(dictionnaire,f,indent=4)
+
 
 
 base = '/Users/erion/Local/python'
+fichier_json = '/Users/erion/Local/python/structure.json'
 structure = {
             'musique': ['alternative','classical','underground'],
             'cine': ['répertoire','recherche','courts'],
@@ -18,3 +23,4 @@ structure = {
             }
 
 creer_dossiers(structure)
+creer_json(fichier_json,structure)
